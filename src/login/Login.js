16e6462a60ef02d6b393ef2365927e266ff25d74
Patrick <pmarks1914@gmail.com>
2024-11-09 00:08:16 +0000

    invoice preview alignment

--- src/login/Login.js
+++ src/login/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-8 mt-10 bg-white rounded-lg shadow-lg">
+    <div className="max-w-md mx-auto p-8 mt-10 bg-white rounded-lg">
       <img src={logo} alt="Logo" className="w-24 mx-auto mb-6" />
       <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">Login</h2>
       
