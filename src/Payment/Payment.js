cefaaea533125bce97e6fab14d567c06a0a0f677
Patrick <pmarks1914@gmail.com>
2024-11-11 22:17:44 +0000

    payment integration

---
+++ src/Payment/Payment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div>
+            
+
+
+        <iframe src='/pay.html'  width="100%" height="950px" title="Iframe Pay">
+          
+        </iframe>
+        </div>
+    );
+};
+
+export default Payment;
